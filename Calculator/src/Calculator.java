babb7ec79f00a092581aa2732f80bb83bffb93dc
MUHAMMAD DIPO PUTRA WANDARA <i16091@ftis>
2018-11-02 14:06:47 +0700

    Initan Commit

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author i16091
+ */
+public class Calculator {
+    public float plus(float a,float b){
+        
+        
+    }
+    public float minus(float a,float b){
+        
+        
+    }
+    public float multiply(float a,float b){
+        
+        
+    }
+    public float divsion(float a,float b){
+        
+        
+    }
+
+}
